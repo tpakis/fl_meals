c1fa4842a4e8b56bbcfd18724e48f70007286c78
Thanos <thanasakis@gmail.com>
2020-12-25 17:42:57 +0100

    small theme improvement

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         textTheme: ThemeData.light().textTheme.copyWith(
               bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
               bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              headline6: TextStyle(fontSize: 24, fontFamily: "RobotoCondensed"),
+              headline6: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, fontFamily: "RobotoCondensed"),
             ),
       ),
       home: CategoriesScreen(),

be101f9808381d75459cae9337c55ece3e8b9bc8
Thanos <thanasakis@gmail.com>
2020-12-25 17:33:14 +0100

    added custom fonts, theme (needs improvment)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import './screens/categories_screen.dart';
 
@@ -9,7 +11,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: "Raleway",
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              headline6: TextStyle(fontSize: 24, fontFamily: "RobotoCondensed"),
+            ),
       ),
       home: CategoriesScreen(),
     );

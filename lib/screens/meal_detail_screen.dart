4a5d38eb9959fcd0bf08b51998f55a32577e722a
Thanos <thanasakis@gmail.com>
2020-12-29 17:28:11 +0100

    finish details screen added divider

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -10,6 +10,66 @@ class MealDetailScreen extends StatelessWidget {
 
   MealDetailScreen(this._mealsProvider);
 
+  Widget _buildSectionTitle(BuildContext context, String title) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(title, style: Theme.of(context).textTheme.headline6),
+    );
+  }
+
+  Widget _buildIngredientsView(BuildContext context, List<String> ingredients) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10)),
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      height: 200,
+      width: 300,
+      child: ListView.builder(
+        itemCount: ingredients.length,
+        itemBuilder: (ctx, index) {
+          return Card(
+            color: Theme.of(context).accentColor,
+            child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                child: Text(ingredients[index])),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildStepsView(BuildContext context, List<String> steps) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10)),
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      height: 200,
+      width: 300,
+      child: ListView.builder(
+        itemCount: steps.length,
+        itemBuilder: (ctx, index) {
+          return Column(
+            children: [
+              ListTile(
+                leading: CircleAvatar(
+                  child: Text("# ${index + 1}"),
+                ),
+                title: Text(steps[index]),
+              ),
+              Divider(),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     setupMeal(context);
@@ -18,8 +78,20 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(_meal.title),
       ),
-      body: Center(
-        child: Text(_meal.title),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(_meal.imageUrl, fit: BoxFit.cover),
+            ),
+            _buildSectionTitle(context, "Ingredients"),
+            _buildIngredientsView(context, _meal.ingredients),
+            _buildSectionTitle(context, "Steps"),
+            _buildStepsView(context, _meal.steps),
+          ],
+        ),
       ),
     );
   }
